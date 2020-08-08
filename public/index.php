39f9753a04bdc5d0b5a34cf250ce7d33193d9f00
oriza <ahmadoriza@gmail.com>
2020-08-08 14:39:15 +0700

    Support env mode on front controller

--- public/index.php
+++ public/index.php
@@ -3,13 +3,31 @@
 /**
  * Front Controller
  * 
- * Titik mulai semuanya.
+ * Everything start here.
+ * 
+ * @author Gemblue
  */
 
-/** Setting error mode. */
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
+/** Environment mode. */
+$config = require '../config/main.php';
+
+switch ($config['environment']) {
+    case 'development':
+        
+        ini_set('display_errors', 1); 
+        ini_set('display_startup_errors', 1); 
+        error_reporting(E_ALL);
+        
+        break;
+        
+    case 'staging':
+        
+        break;
+
+    case 'production':
+        
+        break;
+}
 
 /** Point to bootstrap */
 require '../core/Bootstrap.php';
