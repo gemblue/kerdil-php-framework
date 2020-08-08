95ce4fcab19ec8628a783531f57c9fb76fbd7177
oriza <ahmadoriza@gmail.com>
2020-08-08 10:55:20 +0700

    Make several route sample

---
+++ routes/main.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Routes
+ * 
+ * Routes config, will be read and pointed to the controller method.
+ */
+
+$router->get('/home', 'Home', 'index');
+$router->get('/about', 'Home', 'about');
+
+$router->get('/callback', function(){
+    echo 'It works!';
+});
