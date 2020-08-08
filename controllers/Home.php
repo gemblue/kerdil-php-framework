74d5d31a66c76cd810af667ffc299584fb815d4f
oriza <ahmadoriza@gmail.com>
2020-08-08 15:49:25 +0700

    Trial request library

--- controllers/Home.php
+++ controllers/Home.php
@@ -2,14 +2,25 @@
 
 namespace Controllers;
 
-class Home {
+use Core\Controller;
+use Core\Request;
+
+class Home extends Controller {
+
+    private $request;
 
     public function __construct() {
-        echo 'Home was instantiated .. <br/>';
+    
+        parent::__construct();
+        
+        $this->request = new Request;
     }
 
     public function index() {
-        echo 'Home index ..';
+        
+        $name = $this->request->get('name');
+        
+        echo $name;
     }
     
     public function about() {
