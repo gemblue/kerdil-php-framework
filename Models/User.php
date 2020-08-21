40963aa14471086daf82b726601e40107666d3c2
oriza <ahmadoriza@gmail.com>
2020-08-21 23:28:35 +0700

    Separate search and all method

--- Models/User.php
+++ Models/User.php
@@ -14,14 +14,23 @@ class User {
 
     public function all() {
 
-        $this->qb->select('email, title')
+        $this->qb->select('name, email')->from('users');
+        
+        return $this->qb->result();
+    }
+
+    public function search() {
+
+        $this->qb->select('any_value(email), any_value(title)')
                  ->from('users')
                  ->join('posts', 'posts.user_id = users.id')
-                 ->limit(2);
+                 ->where([
+                    'email' => 'budi@mailinator.com'
+                 ])  
+                 ->groupBy('email')
+                 ->limit(5);
         
-        $users = $this->qb->result();
-
-        return $users;
+        return $this->qb->result();
     }
 
     public function register($email) {
@@ -29,6 +38,23 @@ class User {
         return $this->qb->insert('users', [
             'email' => $email
         ]);
+        
+    }
+
+    public function update() {
+
+        return $this->qb->update('users', [
+            'name' => 'Bambang Saja',
+            'email' => 'bambangsaja@gmail.com'
+        ], [
+            'id' => 1
+        ]);
+        
+    }
+
+    public function delete($id = null) {
+        
+        return $this->qb->delete('users', ['id' => $id]);
         
     }
 }
